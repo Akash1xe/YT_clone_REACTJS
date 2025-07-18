c113bca71636476a19c251346e75a171cd85d1a2
Akash1xe <121711965+Akash1xe@users.noreply.github.com>
2025-07-18 18:18:16 +0530

    Delete src/constants directory

--- src/constants/youtube.jsx
+++
@@ -1,3 +0,0 @@
-export const  API_KEY="AIzaSyBTBbDuOT6x007dIrYeRzje1GELwm6Aee4"
-export const BASE_URL="https://www.googleapis.com/youtube/v3"
-export const YOUTUBE_VIDEO_API=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`
