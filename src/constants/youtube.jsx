1929ef141a308af11e49ed17eed10dade20befb6
Akash Shrivastav <codinchess@gmail.com>
2025-07-18 18:46:09 +0530

    my first commit

--- src/constants/youtube.jsx
+++ src/constants/youtube.jsx
@@ -1,3 +1,3 @@
-export const  API_KEY="AIzaSyBTBbDuOT6x007dIrYeRzje1GELwm6Aee4"
-export const BASE_URL="https://www.googleapis.com/youtube/v3"
-export const YOUTUBE_VIDEO_API=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`
+export const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+export const BASE_URL = process.env.REACT_APP_YOUTUBE_BASE_URL;
+export const YOUTUBE_VIDEO_API = process.env.REACT_APP_YOUTUBE_VIDEO_API;
